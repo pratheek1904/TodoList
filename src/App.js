a1a92dae9e7a0d6c0e458b8458c3721f6a109b66
Pratheek G V <109038768+pratheek1904@users.noreply.github.com>
2023-01-25 12:24:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-import React,{useState} from 'react'
-import Form  from './Form';
+import React from 'react'
+import Form from './Form'
 import './App.css'
 
 const App = () => {
-  const[task,setTask] = useState(""); 
-  const[todos,setTodos] = useState([]);
-
-  const changeHandler =e =>{
-    setTask(e.target.value)
-  }
-  const submitHandler = e => {
-    e.preventDefault();
-    const newTodos = [...todos,task];
-    setTodos(newTodos);
-    if(task===''){
-      alert('Enter a todo first!')
-      return(task);
-    
-    }
-    setTask("");
-  }
-  const deleteHandler = (indexValue) => {
-    const newTodos = todos.filter((todo,index) => index !== indexValue);
-    if(deleteHandler){
-      alert('Would you like to delete?')
-      
-    }
-    setTodos(newTodos);
-
-
-  }
-  return(
-    <div>
-      <center>
-            <h2 >Todo List</h2>
-            <form onSubmit={submitHandler}>
-              <input className='input-text' 
-              type="text" placeholder='Enter the text' 
-              name= "task" value={task} onChange={changeHandler}/> &nbsp;&nbsp;
-              <input className='add' type="submit" value="+" name="Add" />
-            </form>
-            </center>
-            <Form todolist={todos} deleteHandler={deleteHandler}/>
-          
-     </div>
-
+  return (
+    <div className='App'>
+      <Form/>
+    </div>
   )
-
 }
 
 export default App
